1dd61fbdbb0ea330f9bacf8503819f3c3e3b5b0e
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-06-09 22:52:13 +0530

    Create multiply.cpp

---
+++ ds/matrix/multiply.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+
+
+
+class Solution
+{   
+    public:
+    //Function to multiply two matrices.
+    vector<vector<int> > multiplyMatrix( const vector<vector<int> >& A, const vector<vector<int> >& B)
+    {
+        vector<vector<int>>ans;
+        int n1,n2,m1,m2;
+        n1=A.size();
+        m1=A[0].size();
+        n2=B.size();
+        m2=B[0].size();
+        if(m1!=n2)
+            return ans;
+        int t;
+        for(int i=0;i<n1;i++)
+        {
+            vector<int> v;
+            for(int j=0;j<m2;j++)
+            {
+                t=0;
+                for(int k=0;k<n2;k++)
+                {
+                    t+=(A[i][k]*B[k][j]);
+                }
+                v.push_back(t);
+            }
+            ans.push_back(v);
+        }
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > A(row); 
+        for(int i=0; i<row; i++)
+        {
+            A[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>A[i][j];
+            }
+        }
+        cin>> row>> col;
+        vector<vector<int> > B(row);
+        for(int i=0; i<row; i++)
+        {
+            B[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>B[i][j];
+            }
+        }
+        Solution ob;
+        vector<vector<int> > result = ob.multiplyMatrix(A,B);
+
+        if(result.size() == 0)
+            cout<<"-1";
+        else
+        {
+            for (int i = 0; i < result.size(); ++i)
+            {
+                for (int j = 0; j < result[0].size(); ++j)
+                {
+                    cout<<result[i][j]<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+} 
