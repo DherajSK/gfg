2046faee7b5ba8c3eeff171ac4a56138d2a789b2
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-06-24 15:19:08 +0530

    Create firstsetbit.cpp

---
+++ ds/bit/firstsetbit.cpp
@@ -0,0 +1,43 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        int power2=1,count=0;
+        while(power2<=n)
+        {
+            if((n&power2)==power2)
+                return count+1;
+            count++;
+            power2*=2;
+        }
+        return count;
+    
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
