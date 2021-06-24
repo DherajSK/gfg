1f80ccdfbdb2a385fe321c629f5217b16e36b45b
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-06-24 15:58:09 +0530

    Create rightmostdiff.cpp

---
+++ ds/bit/rightmostdiff.cpp
@@ -0,0 +1,42 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        int power2=1,i=0;
+        while(power2<=m || power2<=n)
+        {
+            if((m&power2)!=(n&power2))
+                return i+1;
+            power2*=2;
+            i++;
+        }
+        return i;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+}  
