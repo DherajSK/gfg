a6c157fba361d91d508ad36067ec91cfaa99a712
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-06-24 15:14:40 +0530

    Create bitdifference.cpp

---
+++ ds/bit/bitdifference.cpp
@@ -0,0 +1,40 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        int power2=1,count=0;
+        while(power2<=b || power2<=a)
+        {
+            if((a&power2)!=(b&power2))
+                count++;
+            power2*=2;
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+	int t;
+	cin>>t;// input the testcases
+	while(t--) //while testcases exist
+	{
+		int a,b;
+		cin>>a>>b; //input a and b
+
+        Solution ob;
+		cout<<ob.countBitsFlip(a, b)<<endl;
+	}
+	return 0;
+}
