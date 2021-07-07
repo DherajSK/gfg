c5d46c1c0defa36b78f08f37bc2e20fc19612b9a
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-07-07 17:49:08 +0530

    Create deletemid.cpp

---
+++ ds/linkedlist/deletemid.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to get the middle of the linked list*/
+struct Node *deleteMid(struct Node *head);
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		head = deleteMid(head);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+
+// } Driver Code Ends
+
+
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+Node* deleteMid(Node* head)
+{
+    if(head==NULL)
+        return head;
+    Node *fastptr=head;
+    Node *slowptr=head;
+    Node *prev=head;
+    while(fastptr!=NULL && fastptr->next!=NULL)
+    {
+        fastptr=fastptr->next->next;
+        prev=slowptr;
+        slowptr=slowptr->next;
+    }
+    
+    prev->next=slowptr->next;
+    delete slowptr;
+    
+    return head;
+}
