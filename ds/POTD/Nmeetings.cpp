eb8c5e2a3c07f29acda7b98ba4693bb8d40e0d03
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-09-16 12:01:50 +0530

    Create Nmeetings.cpp

---
+++ ds/POTD/Nmeetings.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        vector<pair<int,int>>meet;
+        for(int i=0;i<n;i++)
+            meet.push_back(make_pair(end[i], start[i]));
+        
+        sort(meet.begin(),meet.end());
+        int count=0;
+        int prev=meet[0].first;
+        for(int i=1;i<n;i++)
+        {
+            if(prev<meet[i].second)
+            {
+                count++;
+                prev=meet[i].first;
+            }
+        }
+        return count+1;
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+} 
